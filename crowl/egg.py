c2db0586823b44ccd20c6c98f81021710c65be46
satok <k0864k@gmail.com>
2018-03-20 09:41:15 +0900

    こみっとです

--- crowl/egg.py
+++ crowl/egg.py
@@ -1,9 +1,24 @@
-'''
-Created on 2018/03/19
+import time
+from selenium import webdriver
 
-@author: k0864
-'''
+browser = webdriver.Chrome()
 
-if __name__ == '__main__':
-    print("hello")
-    print("s")
+url="https://www.victoriassecret.com/bras/shop-all-bras"
+browser.get(url)
+time.sleep(5)
+
+h2s=browser.find_elements_by_class_name("product-card-wrapper")
+if h2s:
+    print("要素ありました")
+    for h2 in h2s:
+
+        print(h2.text)
+    url=    h2.find_element_by_css_selector('h3 > a').get_attribute('href')
+
+
+
+else:
+    print("要素ありませんでした")
+
+#link = browser.find_element_by_class_name("bras")
+#link.click()
